49f51c600106a45d22d0b6f24d9f94c92a64e9ae
WatPow <47085664+WatPow@users.noreply.github.com>
2025-03-25 10:10:16 +0100

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+BACKUP_DIR="/backup"
+LOG_FILE="/var/log/backup.log"
+BKP_FILE="/api/secrets.json"
+
+log() {
+    local log_msg=$1
+    echo "$(/usr/bin/date -u) - ${log_msg}" >> ${LOG_FILE}
+}
+
+backup() {
+    local DATE=$(/usr/bin/date +%s)
+    local DIR="/tmp/${DATE}"
+    /usr/bin/mkdir -m 755 -p "${DIR}" && /usr/bin/chown -R administrator:administrator "${DIR}"
+    log "Copying secrets file to temporary directory"
+    /usr/bin/cp "${BKP_FILE}" "${DIR}/secrets_${DATE}.json"
+    log "Compressing secrets file and moving it to backup directory"
+    /usr/bin/gzip "${DIR}/secrets_${DATE}.json" && /usr/bin/cp "${DIR}/secrets_${DATE}.json.gz" "${BACKUP_DIR}/secrets_${DATE}.json.gz"
+    log "Backup done"
+    log "Removing temporary directory"
+    clear_tmp
+}
+
+clear_tmp() {
+    # Make sure everything that we have done is deleted from /tmp
+    /usr/bin/rm -rf /tmp/*
+    log "Temporary directory cleared"
+    exit 0
+}
+
+log "Backup script started"
+
+if [ ! -d ${BACKUP_DIR} ]; then
+    log "Backup directory does not exist, exiting"
+    exit 1
+fi
+
+if [ -z "$(/usr/bin/ls -A ${BACKUP_DIR})" ]; then
+    log "No backup found, initiating a new backup..."
+    backup
+else
+    NEWEST_BKP=$(/usr/bin/ls -t "${BACKUP_DIR}" | /usr/bin/head -n 1)
+    SHA_NEWEST_BKP=$(/usr/bin/gzip -d -c "${BACKUP_DIR}/${NEWEST_BKP}" | /usr/bin/sha256sum | /usr/bin/awk '{print $1}')
+    SHA_LATEST=$(/usr/bin/sha256sum "${BKP_FILE}" | /usr/bin/awk '{print $1}')
+
+    if [ "${SHA_NEWEST_BKP}" != "${SHA_LATEST}" ]; then
+        log "New backup needed, initiating a new backup..."
+        backup
+    else
+        log "No new backup needed, exiting"
+        exit 0
+    fi
+fi
